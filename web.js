5d7b2fbb4f715f22638f4e3b5e055d5ab58be661
Chamila Amithirigala <amithir2@illinois.edu>
2014-04-12 11:59:04 -0500

    trying to update

---
+++ web.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fs = require('fs');
+var https = require('https');
+var hbs = require('handlebars');
+
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res){
+	fs.readFile('./index.hbs', function(err, data){
+		if(err) throw err;
+		var template = hbs.compile(data.toString());
+		res.send(template());
+	});
+});
+
+var port = Number(process.env.PORT || 5000 );
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
